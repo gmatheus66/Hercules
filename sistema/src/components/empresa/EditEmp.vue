5c4c966bdbf083885b41448fbbd2eead7830454e
gmatheus66 <gmatheus66@hotmail.com>
2020-01-09 03:06:10 -0300

    criando component para editar os dados de uma empresa

---
+++ sistema/src/components/empresa/EditEmp.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="edit">
+      <h2>teste</h2>
+      {{create()}}
+      <p>{{this.$attrs.id}}</p>
+      <p>info - {{this.inf}}</p>
+      <p>error - {{this.err}}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'EditEmp',
+    date(){
+        return{
+           inf: null,
+           err: null,
+        }
+    },
+    mounted: function(){
+           
+    },
+    methods:{
+        create:function(){
+            axios
+            .get('https://apist.herokuapp.com/api/empresa/'+ this.$attrs.id, { useCredentials: true }
+            ).then(response => (this.inf = response.data))
+            .catch(error => (this.err =  error)); 
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
