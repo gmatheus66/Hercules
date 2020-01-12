2f6d0a1f15b0fa8fae44e604bd58f534930b3a3a
gmatheus66 <gmatheus66@hotmail.com>
2020-01-12 17:58:19 -0300

    criando a parte de adicionar e remover um funcionario

---
+++ sistema/src/components/funcionario/AddFuncionario.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="addfuncionario">
+       <div class="addsecoes">
+        <router-link to="/funcionario">
+            <h2 class="secaotitulo">Adicionar Funcionario</h2>
+        </router-link>
+        <div class="line"></div> 
+        </div>
+        <div  v-if="this.data" class="alert alert-dark alertemp" role="alert">
+            <ul v-for="(inf,id) in this.data" v-bind:key="id">
+                <li v-if="!inf.msg">{{inf}}</li>
+                <li v-if="inf.msg" :class="inf.msg.replace(/\s+/g, '')">{{inf.msg}}</li>
+            </ul>
+        </div>
+        <form class="form-group funcformadd" @submit.prevent="savefuncionario">
+            <div class="form-group row">
+                <label for="nome" class="col-sm-2 col-form-label">Nome</label>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control" name="nome" id="nome"  placeholder="Nome" v-model="funcionario.nome">
+                </div>
+                <label for="idade" class="col-sm-1 col-form-label">Idade</label>
+                <div class="col-sm-2">
+                    <input type="number" class="form-control" name="idade" id="idade"  placeholder="Idade" v-model="funcionario.idade">
+                </div>
+            </div>
+            <div class="form-group row">
+                 <label for="endereco" class="col-sm-2 col-form-label">Endereço</label>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control" name="endereco" id="endereco"  placeholder="Endereço" v-model="funcionario.endereco">
+                </div>
+                 <label for="cep" class="col-sm-1 col-form-label">Cep</label>
+                <div class="col-sm-3">
+                    <input type="text" class="form-control" name="cep" id="cep"  placeholder="Cep" v-model="funcionario.cep">
+                </div>
+            </div>
+            <div class="form-group row">
+                 <label for="numero" class="col-sm-2 col-form-label">Numero</label>
+                <div class="col-sm-1">
+                    <input type="number" class="form-control" name="numero" id="numero"  placeholder="Nº" v-model="funcionario.numero_residencia">
+                </div>
+                 <label for="cidade" class="col-sm-1 col-form-label">Cidade</label>
+                <div class="col-sm-3">
+                    <input type="text" class="form-control" name="cidade" id="cidade"  placeholder="Cidade" v-model="funcionario.cidade">
+                </div>
+                 <label for="bairro" class="col-sm-1 col-form-label">Bairro</label>
+                <div class="col-sm-2">
+                    <input  type="text" class="form-control" name="bairro" id="bairro"  placeholder="Bairro" v-model="funcionario.bairro">
+                </div>
+            </div>
+            <div class="form-group row">
+                 <label for="estado" class="col-sm-2 col-form-label">Estado</label>
+                <div class="col-sm-2">
+                    <input type="text" class="form-control" maxlength="2" name="estado" id="estado"  placeholder="Estado" v-model="funcionario.estado">
+                </div>
+                 <label for="pais" class="col-sm-1 col-form-label">Pais</label>
+                <div class="col-sm-2">
+                    <input type="text" class="form-control" maxlength="2" name="pais" id="pais"  placeholder="Pais" v-model="funcionario.pais">
+                </div>
+            </div>
+            <div class="form-group row">
+                 <label for="cargo" class="col-sm-2 col-form-label">Cargo</label>
+                <div class="col-sm-5">
+                    <input type="text" class="form-control" name="cargo" id="cargo"  placeholder="Cargo" v-model="funcionario.cargo">
+                </div>
+            </div>
+             <div class="form-group row">
+             <label for="selectempresa" class="col-sm-2 col-form-label" >Empresa</label>
+                <div class="col-sm-3">
+                    <select class="custom-select" name="estoque_id" id="selectempresa" v-model="funcionario.empresa_id" >
+                            <option selected disabled>Selecione a Empresa</option>
+                            <option v-for="(empresa,id) in this.info"  v-bind:key="id" :value="empresa.id">{{empresa.nome_fantasia}}</option>
+                    </select>
+                </div>
+             </div>
+            <button class="submit funcadd" type="submit">Salvar<i class="large material-icons save">save</i></button> 
+        </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+const queryString = require('query-string');
+export default {
+    name: 'AddFuncionario',
+    data(){
+        return{
+            info: null,
+            error: null,
+            funcionario:{
+                nome: null,
+                idade: null,
+                endereco: null,
+                cep: null,
+                numero_residencia: null,
+                cidade: null,
+                bairro: null,
+                estado: null,
+                pais: null,
+                cargo: null,
+                empresa_id: null
+            },
+            data: null
+        }
+    },
+    mounted(){
+        axios
+      .get('https://apist.herokuapp.com/api/empresa')
+      .then(response => (this.info = response.data))
+      .catch(error => (this.error = error))
+    },
+    methods:{
+        savefuncionario: function(evt){
+            evt.preventDefault();
+            axios.post('https://apist.herokuapp.com/api/funcionario?',queryString.stringify(this.funcionario))
+            .then(response => (this.data = response.data))
+            .catch(error => (this.error = error))    
+            }
+
+    }
+}
+</script>
+
+<style>
+.addfuncionario{
+    margin-top: 5%;
+}
+.funcformadd{
+    margin-top: 3%;
+    margin-left: 10%;
+    margin-right: 5%;
+}
+.funcadd{
+    margin-left: 45%;
+    margin-top: 1%;
+}
+</style>
---
+++ sistema/src/components/funcionario/Funcionario.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="funcionario">
+      <div class="submain">
+        <h2 class="functitulo">Funcionarios</h2>
+        <router-link to="/addfuncionario">
+            <div class="btnaddfunc">
+                <span class="addtext">ADICIONAR</span><img id="icadd" src="../../assets/ic_add.png" alt="icadd">
+            </div>
+        </router-link>
+    </div>
+    <div class="line"></div>
+     <div  v-if="this.delete" class="alert alert-dark alertemp" role="alert">
+        <ul v-for="(inf,id) in this.delete" v-bind:key="id">
+            <li v-if="!inf.msg">{{inf}}</li>
+            <li v-if="inf.msg" :class="inf.msg.replace(/\s+/g, '')">{{inf.msg}}</li>
+        </ul>
+     </div>  
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">Empresa </th>
+                <th scope="col">Nome</th>
+                <th scope="col">Idade</th>
+                <th scope="col">Endereço</th>
+                <th scope="col">Nº</th>
+                <th scope="col">Cep</th>
+                <th scope="col">Cidade</th>
+                <th scope="col"> Bairro</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Pais</th>
+                <th scope="col">Editar</th>
+                <th scope="col">Deletar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(funcionario, id) in this.funcionarios" v-bind:key="id">
+                <td>{{getempresa(funcionario.empresa_id)}}</td>
+                <td>{{funcionario.nome}}</td>
+                <td>{{funcionario.idade}}</td>
+                <td>{{funcionario.endereco}}</td>
+                <td>{{funcionario.numero_residencia}}</td>
+                <td>{{funcionario.cep}}</td>
+                <td>{{funcionario.cidade}}</td>
+                <td>{{funcionario.bairro}}</td>
+                <td>{{funcionario.estado}}</td>
+                <td>{{funcionario.pais}}</td>
+                <td> <router-link :to="'/editmarca/' + funcionario.id"  ><i class="large material-icons">create</i> </router-link></td>
+                <td><i v-on:click="delete_funcionario(funcionario.id, id)" class="large material-icons">delete_forever</i></td>
+            </tr>
+        </tbody>
+    </table>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'funcionario',
+    data(){
+        return{
+            funcionarios: null,
+            error: null,
+            empresas: null,
+            length: null,
+            delete: null
+        }
+    },
+    mounted(){
+        axios
+        .get('https://apist.herokuapp.com/api/funcionario')
+        .then(response => (this.funcionarios = response.data))
+        .catch(error => (this.error = error.data))
+
+        axios
+        .get('https://apist.herokuapp.com/api/empresa')
+        .then(response => (this.empresas = response.data))
+        .catch(error => (this.error = error.data))
+    },
+    methods:{
+        getempresa(id){
+            this.length = this.empresas.length
+
+            for(let i = 0; i < this.length; i++){
+                if(this.empresas[i].id == id){
+                    return this.empresas[0].nome_fantasia
+                }
+            }
+        },
+        delete_funcionario(id, deleteid){
+             axios
+            .delete('https://apist.herokuapp.com/api/funcionario/'+ id)
+            .then(response => (this.delete = response.data))
+            .catch(error =>(this.error = error))
+            
+            this.funcionarios.splice(deleteid, 1)
+        }
+    }
+}
+</script>
+
+<style>
+.line{
+    
+    border-bottom: 5px solid #AEE5E2;
+    margin-left: 2%;
+    margin-top: 1%;
+    border-radius: 10px;
+}
+.funcionario{
+    margin-top: 5%;
+}
+.functitulo{
+    display: inline-block;
+    color: black;
+    margin-left: 2%;
+}
+.btnaddfunc{
+    text-align: center;
+    display: inline;
+    border: 0px solid black;
+    background-color: #B9FAA8;
+    margin-bottom: 1%;
+    padding-top: 2%;
+    padding-left: 1%;
+    padding-bottom: 2%;
+    padding-right: 1%;
+    margin-left: 55%;
+    margin-right: 5%;
+
+}
+.addtext{
+    display: inline;
+    font-size: 1.5em;
+    font-weight: bold;
+}
+#icadd{
+    display: inline;
+    margin-left: 1%;
+}
+
+</style>
