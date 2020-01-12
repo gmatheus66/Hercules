9fbf8d88448373b7e372ae8cad3cbb9aa5cb0570
gmatheus66 <gmatheus66@hotmail.com>
2020-01-12 17:57:32 -0300

    criando a parte de adicionar e remover um fornecedor

---
+++ sistema/src/components/fornecedores/AddFornecedor.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="addfornecedor">
+        <div class="submain">
+        <router-link to="/fornecedor">
+            <h2 class="fornecedortitulo">Adicionar Fornecedor</h2>
+        </router-link>
+    </div>
+    <div class="line"></div>
+
+     <div  v-if="this.info" class="alert alert-dark alertemp" role="alert">
+            <ul v-for="(inf,id) in this.info" v-bind:key="id">
+                <li v-if="!inf.msg">{{inf}}</li>
+                <li v-if="inf.msg" :class="inf.msg.replace(/\s+/g, '')">{{inf.msg}}</li>
+            </ul>
+    </div> 
+    <form class="form-group formaddfornecedor" @submit.prevent="addfornecedor">
+        <div class="form-group row">
+            <label for="razao" class="col-sm-2 col-form-label">Razão Social</label>
+                <div class="col-sm-8">
+                <input type="text" class="form-control" name="razao_social" id="razao" v-model="fornecedor.razao_social" placeholder="Razão Social">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="cnpj" class="col-sm-2 col-form-label">CNPJ</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" name="cnpj" id="cnpj" v-model="fornecedor.cnpj" placeholder="CNPJ">
+            </div>
+        </div>
+         <div class="form-group row">
+            <label for="nome_fantasia" class="col-sm-2 col-form-label">Nome Fantasia</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" name="nome_fantasia" id="nome_fantasia" v-model="fornecedor.nome_fantasia" placeholder="Nome Fantasia">
+            </div>
+        </div>
+         <div class="form-group row">
+            <label for="endereco" class="col-sm-2 col-form-label">Endereço</label>
+            <div class="col-sm-4">
+                <input type="text" class="form-control" name="endereco" id="endereco" v-model="fornecedor.endereco" placeholder="Endereço">
+            </div>
+            <label for="cep" class="col-sm-1 col-form-label">Cep</label>
+            <div class="col-sm-3">
+                <input type="text" class="form-control" name="cep" id="cep" v-model="fornecedor.cep" placeholder="Cep">
+            </div>
+        </div>
+         <div class="form-group row">
+            <label for="numero" class="col-sm-2 col-form-label">Numero</label>
+            <div class="col-sm-1">
+                <input type="number" class="form-control" name="numero_residencia" id="numero" v-model="fornecedor.numero_residencia" placeholder="Nº">
+            </div>
+            <label for="cidade" class="col-sm-1 col-form-label">Cidade</label>
+            <div class="col-sm-3">
+                <input type="text" class="form-control" name="cidade" id="cidade" v-model="fornecedor.cidade" placeholder="Cidade">
+            </div>
+            <label for="bairro" class="col-sm-1 col-form-label">Bairro</label>
+            <div class="col-sm-2">
+                <input type="text" class="form-control" name="bairro" id="bairro" v-model="fornecedor.bairro" placeholder="Bairro">
+            </div>
+        </div>
+         <div class="form-group row">
+            <label for="estado" class="col-sm-2 col-form-label">Estado</label>
+            <div class="col-sm-2">
+                <input type="text" class="form-control" maxlength="2" name="estado" id="estado" v-model="fornecedor.estado" placeholder="Estado">
+            </div>
+            <label for="pais" class="col-sm-1 col-form-label">Pais</label>
+            <div class="col-sm-2">
+                <input type="text" class="form-control" maxlength="2" name="pais" id="pais" v-model="fornecedor.pais" placeholder="Pais">
+            </div>
+        </div>
+        <button class="submit submitfornecedor" type="submit">Salvar<i class="large material-icons save">save</i></button>    
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+const queryString = require('query-string');
+export default {
+    name: 'AddFronecedor',
+    data(){
+        return{
+            info: null,
+            error: null,
+            fornecedor:{
+                razao_social: null,
+                nome_fantasia: null,
+                cnpj: null,
+                endereco: null,
+                cep: null,
+                numero_residencia: null,
+                bairro: null,
+                cidade: null,
+                pais: null,
+                estado: null
+            }
+        }
+    },
+    methods:{
+        addfornecedor(evt){
+            evt.preventDefault();
+            axios.post('https://apist.herokuapp.com/api/fornecedor?', queryString.stringify(this.fornecedor))
+            .then(response => (this.info = response.data))
+            .catch(error => (this.error = error.data))
+        }
+    }
+
+}
+</script>
+
+<style>
+.addfornecedor{
+    margin-top: 5%;
+}
+.formaddfornecedor{
+    margin-top: 5%;
+    margin-left: 15%;
+    margin-right: 4%;
+}
+.submitfornecedor{
+    margin-top: 3%;
+}
+
+</style>
---
+++ sistema/src/components/fornecedores/Fornecedor.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="fornecedor">
+         <div class="submain">
+        <h2 class="fornecedortitulo">Fornecedor</h2>
+        <router-link to="/addfornecedor">
+            <div class="btnaddfornecedor">
+                <span class="addtext">ADICIONAR</span><img id="icadd" src="../../assets/ic_add.png" alt="icadd">
+            </div>
+        </router-link>
+    </div>
+    <div class="line"></div>
+    <div  v-if="this.delete" class="alert alert-dark alertemp" role="alert">
+        <ul v-for="(inf,id) in this.delete" v-bind:key="id">
+            <li v-if="!inf.msg">{{inf}}</li>
+            <li v-if="inf.msg" :class="inf.msg.replace(/\s+/g, '')">{{inf.msg}}</li>
+        </ul>
+     </div>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">Razão Social</th>
+                <th scope="col">CNPJ</th>
+                <th scope="col">N/Fantasia</th>
+                <th scope="col">Endereço</th>
+                <th scope="col">Nº</th>
+                <th scope="col">Cep</th>
+                <th scope="col">Cidade</th>
+                <th scope="col">Bairro</th>
+                <th scope="col">Editar</th>
+                <th scope="col">Deletar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(forne, id) in this.fornecedor" v-bind:key="id">
+                <td>{{forne.razao_social}}</td>
+                <td>{{forne.cnpj}}</td>
+                <td>{{forne.nome_fantasia}}</td>
+                <td>{{forne.endereco}}</td>
+                <td>{{forne.numero_residencia}}</td>
+                <td>{{forne.cep}}</td>
+                <td>{{forne.cidade}}</td>
+                <td>{{forne.bairro}}</td>
+                <td> <router-link :to="'/editfornecedor/' + forne.id"  ><i class="large material-icons">create</i> </router-link></td>
+                <td><i v-on:click="delete_fornecedor(forne.id)" class="large material-icons">delete_forever</i></td>
+
+            </tr>
+        </tbody>
+    </table>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'Fornecedor',
+    data(){
+        return{
+            fornecedor: null,
+            error: null,
+            delete: null
+        }
+    },
+    mounted(){
+        axios
+        .get('https://apist.herokuapp.com/api/fornecedor')
+        .then(response => (this.fornecedor = response.data))
+        .catch(error => (this.error = error.data))
+    },
+    methods:{
+        delete_fornecedor(id){
+            axios
+            .delete('https://apist.herokuapp.com/api/fornecedor/'+ id)
+            .then(response => (this.delete = response.data))
+            .catch(error =>(this.error = error))
+        }
+    }
+}
+</script>
+
+<style>
+.fornecedor{
+    margin-top: 5%;
+}
+.fornecedortitulo{
+    display: inline-block;
+    color: black;
+    margin-left: 2%;
+}
+.btnaddfornecedor{
+    text-align: center;
+    display: inline;
+    border: 0px solid black;
+    background-color: #B9FAA8;
+    margin-bottom: 1%;
+    padding-top: 2%;
+    padding-left: 1%;
+    padding-bottom: 2%;
+    padding-right: 1%;
+    margin-left: 55%;
+    margin-right: 5%;
+
+}
+</style>
